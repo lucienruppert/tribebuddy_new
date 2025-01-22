8e0e6cb6827f8d73f2314bc07a884d377e9e9717
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-01-22 22:34:23 +0100

    cleanup for login

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://devbuddy.hu/tribebuddy',
-  apiUrl: 'http://localhost:3100/tribebuddy',
+  apiUrl: 'http://localhost:3200',
   mailUrl: 'https://luciendelmar.com/backend/api/mail',
   EMAIL_KEY: 'userEmail',
   NAME_KEY: 'userName',
