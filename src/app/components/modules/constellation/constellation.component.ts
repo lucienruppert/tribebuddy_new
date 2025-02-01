4c1cad61bd2e31cb624ab3aeb70b0100e564476d
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-02-01 20:59:54 +0100

    rename constellations service

--- src/app/components/modules/constellation/constellation.component.ts
+++ src/app/components/modules/constellation/constellation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardsService } from '../../../services/cards.service';
+import { CardsService } from '../../../services/constellations.service';
 import { Card } from '../../../types';
 import { cardTranslations } from '../../../shared/translations/card-translations';
 
