51a9405ffcfcc16b0fb820f2d9f0cc5731475b94
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-02-01 20:04:18 +0100

    add card-types component and refactor

---
+++ src/app/components/modules/card-types/card-types.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardsService } from '../../../services/cards.service';
+import { Card } from '../../../types';
+
+@Component({
+  selector: 'app-card-types',
+  templateUrl: './card-types.component.html',
+  styleUrl: './card-types.component.css',
+  standalone: true,
+  imports: [CommonModule],
+})
+export class CardTypesComponent implements OnInit {
+  cards: Card[] = [];
+  private imageExtensions: { [key: string]: string } = {
+    default: 'jpg',
+    wisdomKeepers: 'jpeg',
+    tarot: 'png',
+    osho: 'jpg',
+  };
+
+  constructor(private cardsService: CardsService) {}
+
+  async ngOnInit() {
+    this.cards = await this.cardsService.getCards();
+    this.setDisplayNames();
+  }
+
+  getImagePath(cardName: string): string {
+    const extension = this.imageExtensions[cardName] || 'jpg';
+    return `assets/${cardName}.${extension}`;
+  }
+
+  private setDisplayNames(): void {
+    this.cards.forEach((card: Card) => {
+      switch (card.name) {
+        case 'default': {
+          card.displayName = 'Alap';
+          break;
+        }
+        case 'wisdomKeepers': {
+          card.displayName = 'Bölcsesség-\nőrzők';
+          break;
+        }
+        case 'tarot': {
+          card.displayName = 'Tarot';
+          break;
+        }
+        case 'osho': {
+          card.displayName = 'Osho Zen-tarot';
+          break;
+        }
+      }
+    });
+  }
+}
--- src/app/components/modules/constellation/constellation.component.ts
+++ src/app/components/modules/constellation/constellation.component.ts
@@ -1,56 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CardsService } from '../../../services/cards.service';
-import { Card } from '../../../types';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-constellation',
+  selector: 'app-constellation-types',
+  imports: [],
   templateUrl: './constellation.component.html',
-  styleUrl: './constellation.component.css',
-  standalone: true,
-  imports: [CommonModule],
+  styleUrl: './constellation.component.css'
 })
-export class ConstellationComponent implements OnInit {
-  cards: Card[] = [];
-  private imageExtensions: { [key: string]: string } = {
-    default: 'jpg',
-    wisdomKeepers: 'jpeg',
-    tarot: 'png',
-    osho: 'jpg',
-  };
+export class ConstellationComponent {
 
-  constructor(private cardsService: CardsService) {}
-
-  async ngOnInit() {
-    this.cards = await this.cardsService.getCards();
-    this.setDisplayNames();
-  }
-
-  getImagePath(cardName: string): string {
-    const extension = this.imageExtensions[cardName] || 'jpg';
-    return `assets/${cardName}.${extension}`;
-  }
-
-  private setDisplayNames(): void {
-    this.cards.forEach((card: Card) => {
-      switch (card.name) {
-        case 'default': {
-          card.displayName = 'Alap';
-          break;
-        }
-        case 'wisdomKeepers': {
-          card.displayName = 'Bölcsesség-\nőrzők';
-          break;
-        }
-        case 'tarot': {
-          card.displayName = 'Tarot';
-          break;
-        }
-        case 'osho': {
-          card.displayName = 'Osho Zen-tarot';
-          break;
-        }
-      }
-    });
-  }
 }
