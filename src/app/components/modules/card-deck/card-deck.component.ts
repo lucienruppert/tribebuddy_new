035745fae01aea89a074633bdec9283424a13bf1
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-02-03 22:37:50 +0100

    add card-deck component

---
+++ src/app/components/modules/card-deck/card-deck.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-deck',
+  imports: [],
+  templateUrl: './card-deck.component.html',
+  styleUrl: './card-deck.component.css'
+})
+export class CardDeckComponent {
+
+}
