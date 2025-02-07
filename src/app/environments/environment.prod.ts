446f6eec3b8491e51f8e9867d15a736dfece2799
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-02-07 22:55:51 +0100

    update websocketUrl

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://devbuddy.hu/tribebuddy',
   mailUrl: 'https://luciendelmar.com/backend/api/mail',
-  websocketUrl: 'https://devbuddy.hu/tribebuddy/wsocket',
+  websocketUrl: 'wss://devbuddy.hu/wsocket',
   EMAIL_KEY: 'userEmail',
   NAME_KEY: 'userName',
   SEX_KEY: 'userSex',
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://devbuddy.hu/tribebuddy',
   apiUrl: 'http://localhost:3200',
   mailUrl: 'https://luciendelmar.com/backend/api/mail',
   websocketUrl: 'ws://localhost:3200/wsocket',
