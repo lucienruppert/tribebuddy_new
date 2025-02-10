51cc59ac76f8a0f51cdf0eda8680623d7335d888
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-02-10 19:27:43 +0100

    add geneKeys const

---
+++ constants.ts
@@ -0,0 +1,13 @@
+export const geneKeys = [
+  'lifeWork',
+  'evolution',
+  'radiance',
+  'purpose',
+  'attraction',
+  'iq',
+  'eq',
+  'sq',
+  'vocation',
+  'culture',
+  'pearl',
+] as const;
