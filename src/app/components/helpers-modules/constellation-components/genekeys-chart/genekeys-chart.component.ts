b50bd51679ac4b238c5e4d016cd17bba62db0c35
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-02-13 20:27:29 +0100

    style outer circle

--- src/app/components/helpers-modules/constellation-components/genekeys-chart/genekeys-chart.component.ts
+++ src/app/components/helpers-modules/constellation-components/genekeys-chart/genekeys-chart.component.ts
@@ -10,4 +10,6 @@ import { CommonModule } from '@angular/common';
 })
 export class GenekeysChartComponent {
   radius = 350; 
+  outerCircleLines = Array(48).fill(0); // Creates array of 48 elements for the outer circle lines
+  Math = Math; // Make Math available in template
 }
