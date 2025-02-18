8ba86a33cd6a92d2f6f5dddf49301722fc0855c7
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-02-18 18:26:52 +0100

    add session wrapper

---
+++ src/app/components/helper-modules/session/session-wrapper/session-wrapper.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-wrapper',
+  imports: [],
+  templateUrl: './session-wrapper.component.html',
+  styleUrl: './session-wrapper.component.css'
+})
+export class SessionWrapperComponent {
+
+}
