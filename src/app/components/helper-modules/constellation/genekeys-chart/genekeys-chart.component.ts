1c6e66c8fb5d512e98a27c34fb5efe4d1db9b4f8
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-03-01 19:21:09 +0100

    change card size

--- src/app/components/helper-modules/constellation/genekeys-chart/genekeys-chart.component.ts
+++ src/app/components/helper-modules/constellation/genekeys-chart/genekeys-chart.component.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { onChart } from '../../../../types';
 import { WebsocketService } from '../../../../services/websocket.service';
 import { Subscription } from 'rxjs';
-import { OnChartUpdateForwardMessage, UpdateOnChartMessage } from '../../../../types-websocket';
+import {
+  OnChartUpdateForwardMessage,
+  UpdateOnChartMessage,
+} from '../../../../types-websocket';
 
 @Component({
   selector: 'app-genekeys-chart',
@@ -29,34 +32,32 @@ export class GenekeysChartComponent implements OnInit, OnDestroy {
 
   private wsSubscription: Subscription | null = null;
 
-  constructor(
-    private websocketService: WebsocketService
-  ) {
-
-  }
+  constructor(private websocketService: WebsocketService) {}
 
   ngOnInit(): void {
-    this.wsSubscription = this.websocketService.getMessages().subscribe(message => {
-      if (
-        message &&
-        typeof message === 'object' &&
-        'type' in message &&
-        message.type === 'onChartUpdateForward' &&
-        'onChart' in message
-      ) {
-        const updateMessage = message as OnChartUpdateForwardMessage;
-        console.log('Received updateOnChart message:', updateMessage);
+    this.wsSubscription = this.websocketService
+      .getMessages()
+      .subscribe(message => {
+        if (
+          message &&
+          typeof message === 'object' &&
+          'type' in message &&
+          message.type === 'onChartUpdateForward' &&
+          'onChart' in message
+        ) {
+          const updateMessage = message as OnChartUpdateForwardMessage;
+          console.log('Received updateOnChart message:', updateMessage);
 
-        // Transform the onChart data with name mapping
-        this.onChart = Object.entries(updateMessage.onChart).reduce(
-          (acc, [key, value]) => {
-            acc[this.nameMapping[key]] = value;
-            return acc;
-          },
-          {} as onChart
-        );
-      }
-    });
+          // Transform the onChart data with name mapping
+          this.onChart = Object.entries(updateMessage.onChart).reduce(
+            (acc, [key, value]) => {
+              acc[this.nameMapping[key]] = value;
+              return acc;
+            },
+            {} as onChart
+          );
+        }
+      });
   }
 
   ngOnDestroy(): void {
@@ -72,76 +73,76 @@ export class GenekeysChartComponent implements OnInit, OnDestroy {
   @Input() sessionId!: string;
 
   cardPositions = [
-    { name: 'SQ', x: 360 - 10, y: 345 - 13, width: 115, height: 156 },
+    { name: 'SQ', x: 360 - 10, y: 345 - 13, width: 92, height: 125 },
     {
       name: 'Fejlődésed útja',
       x: 400 + 350 - 53, // Moved additional 5px right
       y: 346 - 13,
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
     {
       name: 'Ragyogásod',
       x: 400 - 350 - 53, // Moved 5px right
       y: 346 - 13,
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
     {
       name: 'Tennivalód az életben',
       x: 360 - 10,
       y: 400 - 350 - 51, // Moved additional 5px down
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
     {
       name: 'Életcélod',
       x: 360 - 10,
       y: 400 + 350 - 90, // Moved 5px down
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
     {
       name: 'Gyöngy',
       x: 360 - 10,
       y: 400 - 350 * 0.5 - 78,
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
     {
       name: 'Vonzásmeződ',
       x: 360 - 10,
       y: 400 + 350 * 0.5 - 78,
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
     {
       name: 'EQ',
       x: 400 + 350 * 0.5 * 0.866 - 53, // Moved 5px right
       y: 400 + 350 * 0.5 * 0.5 - 78,
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
     {
       name: 'IQ',
       x: 400 - 350 * 0.5 * 0.866 - 53, // Moved 5px right
       y: 400 + 350 * 0.5 * 0.5 - 78,
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
     {
       name: 'Kultúra',
       x: 400 + 350 * 0.5 * 0.866 - 53, // Moved 5px right
       y: 400 - 350 * 0.5 * 0.5 - 78,
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
     {
       name: 'Mag/Hivatás',
       x: 400 - 350 * 0.5 * 0.866 - 53, // Moved 5px right
       y: 400 - 350 * 0.5 * 0.5 - 78,
-      width: 115,
-      height: 156,
+      width: 92,
+      height: 125,
     },
   ];
 
