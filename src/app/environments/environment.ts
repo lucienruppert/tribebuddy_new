957d76069da41be6dcefc909c20c3524bf4cb7d3
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-03-01 18:50:40 +0100

    update path

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3200',
   mailUrl: 'https://luciendelmar.com/backend/api/mail',
-  websocketUrl: 'ws://localhost:3200/wsocket',
+  websocketUrl: 'ws://localhost:3200/tribesocket',
 };
